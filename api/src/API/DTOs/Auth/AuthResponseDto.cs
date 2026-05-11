9e4c5d407417e023f3afe6c10d2f20befab51e1c
PratyushSinha15 <prayushsinha8268@gmail.com>
2026-05-11 23:51:51 +0530

    Added DTOs For authResponse and RegisterationRequest

---
+++ api/src/API/DTOs/Auth/AuthResponseDto.cs
@@ -0,0 +1,12 @@
+namespace API.DTOs.Auth;
+
+public class AuthResponseDto
+{
+    public string UserId { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string? LastName { get; set; }
+    public string Token { get; set; } = null!;
+}
