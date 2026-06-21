e987437a3068aaa17b383bff793ab79fe435dc96
PratyushSinha15 <prayushsinha8268@gmail.com>
2026-06-21 16:17:27 +0530

    Added History model, service and worked on history ui

--- ui/src/app/core/services/analysis.ts
+++ ui/src/app/core/services/analysis.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { DetectRequest } from '../../models/DetectRequest';
 import { DetectResponse } from '../../models/DetectResponse';
 import { Observable } from 'rxjs/internal/Observable';
+import { HistoryItem } from '../../models/HistoryModel';
 
 @Injectable({
   providedIn: 'root',
@@ -17,5 +18,11 @@ export class Analysis {
       `${this.apiUrl}/detect`,
       request
     );
+  }
+
+  getHistory(): Observable<HistoryItem[]> {
+    return this.http.get<HistoryItem[]>(
+      `${this.apiUrl}/history`
+    )
   }
 }
